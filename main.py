67d5ea565adf3eb6019f8815895babc7d5ea9d6b
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2020-10-20 11:07:26 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-
+light.set_pixel_color(0, light.rgb(250, 0, 0))
+light.set_pixel_color(1, light.rgb(0, 0, 250))
+light.set_pixel_color(2, light.rgb(0, 250, 0))
--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
-
+light.setPixelColor(0, light.rgb(250, 0, 0))
+light.setPixelColor(1, light.rgb(0, 0, 250))
+light.setPixelColor(2, light.rgb(0, 250, 0))
